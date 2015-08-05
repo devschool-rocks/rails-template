67e128f037ce7aec1dcea7d2f20669872cf3bd20
Joe Sak <joemsak@users.noreply.github.com>
2015-08-04 20:36:39 -0500

    Update gem versions

--- default.rb
+++ default.rb
@@ -1,26 +1,26 @@
-gem 'underscore-rails', '~> 1.7'
-gem 'foundation-rails', '~> 5.3'
-gem 'friendly_id', '~> 5.0'
+gem 'underscore-rails', '~> 1.8'
+gem 'foundation-rails', '~> 5.5'
+gem 'friendly_id', '~> 5.1'
 
 gem_group :test do
   gem 'pry-rails', '~> 0.3'
   gem 'pry-nav', '~> 0.2'
-  gem 'rspec-rails', '~> 3.0'
-  gem 'database_cleaner', '~> 1.3'
+  gem 'rspec-rails', '~> 3.3'
+  gem 'database_cleaner', '~> 1.4'
   gem 'launchy', '~> 2.4'
-  gem 'capybara-webkit', '~> 1.2'
-  gem 'factory_girl_rails', '~> 4.4'
+  gem 'capybara-webkit', '~> 1.6'
+  gem 'factory_girl_rails', '~> 4.5'
   gem 'spring-commands-rspec', '~> 1.0'
 end
 
 gem_group :test, :development do
-  gem 'quiet_assets', '~> 1.0'
-  gem 'dotenv-rails'
+  gem 'quiet_assets', '~> 1.1'
+  gem 'dotenv-rails', '~> 2.0'
 end
 
 gem_group :production do
   gem 'rails_12factor', '~> 0.0'
-  gem 'unicorn', '~> 4.3'
+  gem 'unicorn', '~> 4.9'
 end
 
 run 'bundle install'
